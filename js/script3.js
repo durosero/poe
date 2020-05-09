30d4c0bb039eea8263019c7b52451af2efbdeb31
Duvan Rosero <duvanrosero1995@gmail.com>
2020-05-09 09:42:38 -0500

    guardamos los vengadores en la memoria

--- js/script3.js
+++ js/script3.js
@@ -1,19 +1,24 @@
-console.log("Hoola mundo");
 var formHeroes = document.getElementById('formHeroes');
 
 formHeroes.addEventListener('submit', function(event) {
     event.preventDefault();
     var formData = new FormData(formHeroes);
-    console.log(formData.get('nombre'));
+    // var heroe = {
+    //     nombre: formData.get('nombre'),
+    //     alias: formData.get('alias'),
+    //     poder: formData.get('poder'),
+    //     color: formData.get('color'),
+    // };
+    var heroe = Object.fromEntries(formData);
 
-    var heroe = {
-        nombre: formData.get('nombre'),
-        alias: formData.get('alias'),
-        poder: formData.get('poder'),
-        color: formData.get('color'),
-    };
+    var vengadores = [];
 
-    console.log(heroe);
-
+    if (localStorage.getItem('vengadores') == null) {
+        vengadores.push(heroe);
+    } else {
+        vengadores = JSON.parse(localStorage.getItem('vengadores'));
+        vengadores.push(heroe);
+    }
+    localStorage.setItem('vengadores', JSON.stringify(vengadores));
 
 });
